b757a480e814ac08ccd235669dd7fb174a3ba88c
fredericocascao <fredcascas@hotmail.com>
2025-07-03 10:05:16 -0300

    att ex 022

--- ex022.py
+++ ex022.py
@@ -1,9 +1,9 @@
 nome = input('Qual é o seu nome? ')
 
-print(nome.upper())  # converte o nome para maiúsculas
-print(nome.lower())  # converte o nome para minúsculas
-print(len(''.join(nome.split()))) # remove espaços do nome
+print('Seu nome em maiúscula é: ', nome.upper())  # converte o nome para maiúsculas
+print('Seu nome em minúsculas é: ', nome.lower())  # converte o nome para minúsculas
+print('O total de caracteres (sem espaços) é: ', len(''.join(nome.split()))) # remove espaços do nome
 
 dividido = nome.split()
-print(len(dividido[0]))  # divide o nome em uma lista de palavras
+print('O tamanho do primeiro nome é: ', len(dividido[0]))  # divide o nome em uma lista de palavras
 
