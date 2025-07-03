9bb78fb09ae65e5829030fffb9ad83b3d40e52cd
fredericocascao <fredcascas@hotmail.com>
2025-07-03 10:37:22 -0300

    at 024

--- ex024.py
+++ ex024.py
@@ -1,4 +1,4 @@
-cidade = input('Digite o nome de uma cidade: ')
+cidade = input('Digite o nome de uma cidade: ').strip() # Remove espaços extras no início e no final
 
 print('A cidade digitada foi:', cidade)
 print('A cidade começa com "Santo"?', cidade.lower().startswith('santo'))
